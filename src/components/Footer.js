538e7227f1e28e89e398ba730ed4c39a2c96957e
vansh2409 <75140413+vansh2409@users.noreply.github.com>
2021-12-14 18:56:54 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="d-flex align-items-center justify-content-center black-body">
+            <p className="text-muted mt-3 black-body">
+                A{" "}
+                <a
+                    className="no-decoration white"
+                    href="https://github.com/dunleavyjack"
+                >
+                    Vansh Choudhary
+                </a> Production{" "}
+                :)
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <Link className="navbar-brand nav-title brand-text" to="/">
+                    <span className="pinkish">Moodify</span>
+                </Link>
+                <div className="navbar-nav ml-auto">
+                    <Link
+                        className="navbar-brand nav-title brand-text"
+                        to="/about"
+                    >
+                        about
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
